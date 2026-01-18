36d6218c75a14df71622c8637a47dd0000a0e1b6
AgosArias <agosarias99@gmail.com>
2026-01-18 17:19:41 +0100

    <feat>(minishell.h): crea minishell.h una estructura t_cmd

--- include/minishell.h
+++ include/minishell.h
@@ -3,16 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jgodoy-m <jgodoy-m@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: aarias-d <aarias-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 13:01:41 by jgodoy-m          #+#    #+#             */
-/*   Updated: 2026/01/16 21:29:31 by jgodoy-m         ###   ########.fr       */
+/*   Updated: 2026/01/18 17:15:12 by aarias-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-
+typedef struct s_cmd
+{
+    char            **argv;   
+    struct s_redir   *redirs;
+    struct s_cmd     *next;
+} t_cmd;
 
 #endif
