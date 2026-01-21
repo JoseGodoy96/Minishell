45b246a4540c5b9d484528018fe80faeae0c148e
Jose Maria <josemariagodoy1996@gmail.com>
2026-01-21 19:34:55 +0100

    <feat>(lexer_utils_3.c, lexer_utils_2.c, lexec.c): merge de la rama Operators borro operators voy con las reglas del parser

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aarias-d <aarias-d@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: jgodoy-m <jgodoy-m@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/12 13:01:41 by jgodoy-m          #+#    #+#             */
-/*   Updated: 2026/01/20 20:37:49 by aarias-d         ###   ########.fr       */
+/*   Created: 2026/01/21 19:30:34 by jgodoy-m          #+#    #+#             */
+/*   Updated: 2026/01/21 19:30:39 by jgodoy-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,39 +45,41 @@ typedef struct s_token
 }	t_token;
 
 // lexer_utils_1.c
-int		is_space(char c);
-void	skip_spaces(const char *str, int *i);
-int		is_quote(char c);
-t_token	*token_new(t_toktype type, char *value);
-void	token_add_back(t_token **lst, t_token *node);
+int			is_space(char c);
+void		skip_spaces(const char *str, int *i);
+int			is_quote(char c);
+t_token		*token_new(t_toktype type, char *value);
+void		token_add_back(t_token **lst, t_token *node);
 
 // lexer_utils_2.c
-char	*read_unquoted(const char *str, int *i);
-char	*read_quoted(const char *str, int *i, char quote);
-char	*str_join_free(char *a, char *b);
-void	token_free_all(t_token *lst);
-void	token_free_all(t_token *lst);
+char		*read_unquoted(const char *str, int *i);
+char		*read_quoted(const char *str, int *i, char quote);
+char		*str_join_free(char *a, char *b);
+void		token_free_all(t_token *lst);
+void		token_free_all(t_token *lst);
 
 // lexer_utils_3.c
-char	*read_word(const char *str, int *i);
+char		*read_word(const char *str, int *i);
+int			is_metachar(char c);
+t_toktype	get_op_type(const char *str, int *i);
 
 // lexer.c
-t_token	*lexer(const char *line);
+t_token		*lexer(const char *line);
 
 // execute.c 
-int		ft_execute(t_cmd *cmd, char **envp);
+int			ft_execute(t_cmd *cmd, char **envp);
 
 // exec_single.c 
-int		ft_exec_single(t_cmd *cmd, char **envp);
+int			ft_exec_single(t_cmd *cmd, char **envp);
 
 // exec_pipeline.c 
-int		ft_exec_pipeline(t_cmd *cmd, char **envp);
+int			ft_exec_pipeline(t_cmd *cmd, char **envp);
 
 // path_utils.c 
-char	*ft_join_path(char *dir, char *cmd);
-char	*ft_get_path(char **envp, char *cmd);
+char		*ft_join_path(char *dir, char *cmd);
+char		*ft_get_path(char **envp, char *cmd);
 
 // free.c 
-void	ft_free_matriz(char **matriz);
+void		ft_free_matriz(char **matriz);
 
 #endif
